f539522369f94c700a8218050271d380053dc7a0
Alexandra <Alexandra.kunitskaya@gmail.com>
2019-09-25 18:01:37 +0300

    init: start implementing multiply task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  const result = +first * +second;
+  if (Number.isSafeInteger(result)) {
+    return result.toString();
+  } else {
+    const bigger = first > second ? first : second;
+    const smaller = first < second ? first : second;
+    const multiplications = [];
+    const biggerDigits = bigger.split('');
+    const smallerDigits = smaller.split('');
+
+    for(let i = 0; i < smallerDigits.length; i++) {
+      let smallDigitMulRes = 0;
+      biggerDigits.forEach(digit => {
+        const res = smallDigitMulRes[i] * digit;
+        smallDigitMulRes += res;
+      });
+      multiplications.push(smallDigitMulRes);
+    }
+  }
 }
