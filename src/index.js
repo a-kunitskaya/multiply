2b22e81e647d28e29d6371dc4657fb4748b6cfac
Alexandra <Alexandra.kunitskaya@gmail.com>
2019-10-03 08:48:52 +0300

    feat: implement multiplying algorithm for big numbers

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 module.exports = function multiply(first, second) {
-  const result = +first * +second;
-  if (Number.isSafeInteger(result)) {
-    return result.toString();
-  } else {
-    const bigger = first > second ? first : second;
-    const smaller = first < second ? first : second;
-    const multiplications = [];
-    const biggerDigits = bigger.split('');
-    const smallerDigits = smaller.split('');
+    const multiplication = +first * +second;
 
-    for(let i = 0; i < smallerDigits.length; i++) {
-      let smallDigitMulRes = 0;
-      biggerDigits.forEach(digit => {
-        const res = smallDigitMulRes[i] * digit;
-        smallDigitMulRes += res;
-      });
-      multiplications.push(smallDigitMulRes);
+    //first * second = in range MAX_SAFE_INTEGER (2 53), MIN_SAFE_INTEGER (-2 53)
+    if (Number.isSafeInteger(multiplication)) return (multiplication).toString();
+
+    const firstArr = first.split('').reverse();
+    const secondArr = second.split('').reverse();
+    let result = [];
+
+    for (let i = 0; firstArr[i] >= 0; i++) {
+        for (let j = 0; secondArr[j] >= 0; j++) {
+            if (!result[i + j]) result[i + j] = 0;
+
+            //put digit from first * digit from second result to arr
+            result[i + j] += firstArr[i] * secondArr[j];
+        }
     }
-  }
-}
+
+    for (let i = 0; result[i] >= 0; i++) {
+        if (result[i] >= 10) {
+            if (!result[i + 1]) result[i + 1] = 0;
+
+            // take integer, leaving the remainder out, add it to the next mul value
+            result[i + 1] += parseInt(result[i] / 10);
+            // save remainder in the initial mul value's place
+            result[i] %= 10;
+        }
+    }
+
+    result.reverse();
+    for (let i = 0; i < result.length; i++) {
+        if (result[i] === 0) {
+            result.splice(i--, 1);
+        } else {
+            i = result.length * 2;
+        }
+    }
+
+    return result.join('');
+};
